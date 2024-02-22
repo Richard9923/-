76dc5f693ebd2a488ce576476dc39f18ca145b1c
Richard <programmingislife23@gmail.com>
2024-02-22 20:28:10 -0300

    treinando com filter

--- app.js
+++ app.js
@@ -3,3 +3,12 @@
 /* setTimeout( () => {
     console.log("Hello")
 }, 3000) */
+
+/* recurso para filtrar determinados valores */
+
+/* lista.filter( n => {
+    return n < 10
+
+    "vai ser retornado uma lista com todos os valores true"
+})
+*/
